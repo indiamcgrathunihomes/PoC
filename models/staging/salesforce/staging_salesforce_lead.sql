aa0379b3458b2c6cf960bd119731cb897c43c37a
India McGrath <india.mcgrath@unihomes.co.uk>
2025-07-15 14:00:27 +0000

    corrected whitespace in staging_salesforce_lead.sql

--- models/staging/salesforce/staging_salesforce_lead.sql
+++ models/staging/salesforce/staging_salesforce_lead.sql
@@ -2,16 +2,11 @@
 
 
 -- Only clean up I am doing is to filter for just Landlord/Agent accounts and to remove trailing '_c' from field names
-    SELECT
+ SELECT
         id,
-        name,
+        company,
         associated_city_c AS associated_city,
-        billing_postal_code,
-        total_student_portfolio_c AS total_student_portfolio,
-        account_type_c AS account_type,
-        record_type_id,
-        date_closed_c AS date_closed,
-        date_won_c AS date_won,
+        postal_code AS postcode,
+        percentage_student_c AS total_student_portfolio,
     FROM
-      {{ source('salesforce', 'account') }}
-    
+        {{ source('salesforce', 'lead') }}

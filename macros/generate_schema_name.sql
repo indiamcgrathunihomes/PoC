362918dda6ca049388ea4842dfdc611ec8e4cae0
India McGrath <india.mcgrath@unihomes.co.uk>
2025-07-15 16:09:17 +0000

    Updated generate_schema_name macro to create separate schemas in development environment for each user

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,12 +1,18 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-  
-  {# 1) If you’ve set +schema in your project or model, use exactly that #}
+  {# Read the DBT_ENV var you’ll define in each Cloud environment #}
+  {% set env = env_var('DBT_ENV', default=target.schema) %}
+
+  {# 1) If you’ve set +schema, decide based on env #}
   {% if custom_schema_name is not none %}
-    {{ custom_schema_name }}
+    
+    {%- if env == 'prod' -%}
+      {{ custom_schema_name }}
+    {%- else -%}
+      {{ env ~ '_' ~ custom_schema_name }}
+    {%- endif %}
 
-  {# 2) Otherwise fall back to the default (target schema) #}
+  {# 2) No +schema? Fall back to env (or target.schema) #}
   {% else %}
-    {{ target.schema }}
+    {{ env }}
   {% endif %}
-
 {%- endmacro %}

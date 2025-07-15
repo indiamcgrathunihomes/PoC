559d7e917e1419aacbcf6ea6ef73b4f32942aae4
India McGrath <india.mcgrath@unihomes.co.uk>
2025-07-15 14:11:04 +0000

    another typo fix on staging_salesforce_lead.sql

--- models/staging/salesforce/staging_salesforce_lead.sql
+++ models/staging/salesforce/staging_salesforce_lead.sql
@@ -1,13 +1,13 @@
 
 
 
--- Only clean up I am doing is to filter for just Landlord/Agent accounts and to remove trailing '_c' from field names
+-- Only clean up I am doing is remove trailing '_c' from field names
  SELECT
         id,
         company,
         associated_city_c AS associated_city,
-        postal_code AS postcode,
-        percentage_student_c AS total_student_portfolio,
+        postal_code,
+        percentage_student_c AS percentage_student,
         record_type_id,
         main_contact_c AS main_contact,
         status

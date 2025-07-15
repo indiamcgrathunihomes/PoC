412a60f881ed941e8b5b5ed54af7d2c61fdd32f7
India McGrath <india.mcgrath@unihomes.co.uk>
2025-07-15 14:09:54 +0000

    Extended staging_salesforce_lead.sql

--- models/staging/salesforce/staging_salesforce_lead.sql
+++ models/staging/salesforce/staging_salesforce_lead.sql
@@ -8,5 +8,8 @@
         associated_city_c AS associated_city,
         postal_code AS postcode,
         percentage_student_c AS total_student_portfolio,
+        record_type_id,
+        main_contact_c AS main_contact,
+        status
     FROM
         {{ source('salesforce', 'lead') }}

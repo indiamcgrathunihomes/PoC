cbaaae5664a507f8ce8de57154e0709f2769bee8
William Everton <william.everton@unihomes.co.uk>
2025-07-16 14:49:10 +0000

    Adding in the Opportunity Line Item into this model to get the Student Portfolio value.

--- models/staging/salesforce/staging_salesforce_opportunity.sql
+++ models/staging/salesforce/staging_salesforce_opportunity.sql
@@ -1,9 +1,33 @@
 -- Only clean up I am doing is to filter for just Landlord/Agent accounts and to remove trailing '_c' from field names 
-SELECT
+WITH opportunity_quantity AS (
+    SELECT
+        opportunity_id,
+        SUM(quantity) as total_student_portfolio
+    FROM 
+        {{ source('salesforce', 'opportunity_products') }}
+    WHERE
+        portfolio_type_c = 'Student'
+    GROUP BY
+        opportunity_id
+),
+
+opportunity AS (
+    SELECT
         id,
         name,
         account_id,
-        stage_name,
-        total_opportunity_quantity
-     FROM
-      {{ source('salesforce', 'opportunity') }}
+        stage_name
+    FROM 
+        {{ source('salesforce', 'opportunity') }}
+)
+
+SELECT
+    op.id,
+    op.name,
+    op.account_id,
+    op.stage_name,
+    COALESCE(oq.total_student_portfolio,0) AS total_student_portfolio
+FROM
+    opportunity op
+LEFT JOIN 
+    opportunity_quantity oq ON op.id = oq.opportunity_id

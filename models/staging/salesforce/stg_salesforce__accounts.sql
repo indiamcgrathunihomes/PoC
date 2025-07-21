a6e378da0f644d4d46705400c34b53066712c8e7
India McGrath <india.mcgrath@unihomes.co.uk>
2025-07-21 09:53:26 +0000

    Updated stg_salesforce__accounts.sql to pull from

--- models/staging/salesforce/stg_salesforce__accounts.sql
+++ models/staging/salesforce/stg_salesforce__accounts.sql
@@ -1,22 +1,10 @@
 with
 
-    source as (select * from {{ source("salesforce", "account") }}),
+    source as ( select * from {{ source("salesforce", "account") }}),
 
-    source_excluding_deleted_records as (select * from source where is_deleted = false),
+    excluding_deleted_records as (select * from source where is_deleted = false),
 
-    renamed as (
-        select
-            id,
-            name,
-            associated_city_c as associated_city,
-            billing_postal_code,
-            total_student_portfolio_c as total_student_portfolio,
-            account_type_c as account_type,
-            record_type_id,
-            date_closed_c as date_closed,
-            date_won_c as date_won
-        from source_excluding_deleted_records
-    )
+    snake_case_field_names_and_clean_timestamps as ( select  {{ select_fields_structured('salesforce', 'account') }} from excluding_deleted_records)
 
 select *
-from renamed
+from snake_case_field_names_and_clean_timestamps

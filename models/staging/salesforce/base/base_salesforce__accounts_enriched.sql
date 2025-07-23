e707a820073f4b8936d71ac25dc54fbbe50ccd9f
India McGrath <india.mcgrath@unihomes.co.uk>
2025-07-23 15:15:38 +0000

    Updated base_salesforce__accounts_enriched.sql to include user pull

--- models/staging/salesforce/base/base_salesforce__accounts_enriched.sql
+++ models/staging/salesforce/base/base_salesforce__accounts_enriched.sql
@@ -7,10 +7,10 @@ record_types as (
     from {{ ref('stg_salesforce__record_types') }}
 ),
 
-/*users as (
+users as (
     select user_id, name
-    from {% raw %}{{ ref('stg_salesforce__users') }}{% endraw %}
-), */
+    from {{ ref('stg_salesforce__users') }}
+), 
 
 parent_accounts as (
     select account_id, name as parent_account_name
@@ -27,21 +27,21 @@ select
 
     -- Replacing IDs with names
     rt.record_type_name,
-   /*owner.name as owner_name,
+    owner.name as owner_name,
     created_by.name as created_by_name,
-    last_modified_by.name as last_modified_by_name,*/
+    last_modified_by.name as last_modified_by_name,
     parent.parent_account_name,
     master.master_record_name
 
 from accounts acc
 left join record_types rt
     on acc.record_type_id = rt.record_type_id
-/*left join users owner
+left join users owner
     on acc.owner_id = owner.user_id
 left join users created_by
     on acc.created_by_id = created_by.user_id
 left join users last_modified_by
-    on acc.last_modified_by_id = last_modified_by.user_id */
+    on acc.last_modified_by_id = last_modified_by.user_id 
 left join parent_accounts parent
     on acc.parent_id = parent.account_id
 left join master_accounts master

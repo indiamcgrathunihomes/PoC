f56d929080762e9298f3766e7b8916e2cb63dcca
India McGrath <india.mcgrath@unihomes.co.uk>
2025-07-23 16:19:37 +0000

    Switched base x staging structure so that base pulls from multiple sources and then staging pulls from base

--- models/staging/salesforce/base/base_salesforce__accounts_enriched.sql
+++ models/staging/salesforce/base/base_salesforce__accounts_enriched.sql
@@ -1,25 +1,26 @@
 with accounts as (
-    select * from {{ ref('stg_salesforce__accounts') }}
+    select *
+    from {{ source('salesforce', 'account') }}
 ),
 
 record_types as (
-    select record_type_id, name as record_type_name
-    from {{ ref('stg_salesforce__record_types') }}
+    select id as record_type_id, name as record_type_name
+    from {{ source('salesforce', 'record_type') }}
 ),
 
 users as (
-    select user_id, name
-    from {{ ref('stg_salesforce__users') }}
-), 
+    select id as user_id, name
+    from {{ source('salesforce', 'user') }}
+),
 
 parent_accounts as (
-    select account_id, name as parent_account_name
-    from {{ ref('stg_salesforce__accounts') }}
+    select id as account_id, name as parent_account_name
+    from {{ source('salesforce', 'account') }}
 ),
 
 master_accounts as (
-    select account_id, name as master_record_name
-    from {{ ref('stg_salesforce__accounts') }}
+    select id as master_record_id, name as master_record_name
+    from {{ source('salesforce', 'account') }}
 )
 
 select
@@ -30,8 +31,7 @@ select
     owner.name as owner_name,
     created_by.name as created_by_name,
     last_modified_by.name as last_modified_by_name,
-    parent.parent_account_name,
-    master.master_record_name
+    parent.parent_account_name
 
 from accounts acc
 left join record_types rt
@@ -41,8 +41,6 @@ left join users owner
 left join users created_by
     on acc.created_by_id = created_by.user_id
 left join users last_modified_by
-    on acc.last_modified_by_id = last_modified_by.user_id 
+    on acc.last_modified_by_id = last_modified_by.user_id
 left join parent_accounts parent
     on acc.parent_id = parent.account_id
-left join master_accounts master
-    on acc.master_record_id = master.account_id

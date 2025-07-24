21a1003500c6caee51621ce7846d37a3de5ad48c
India McGrath <india.mcgrath@unihomes.co.uk>
2025-07-24 11:12:19 +0000

    Updated naming and comments in stg_salesforce__accounts.sql

--- models/staging/salesforce/stg_salesforce__accounts.sql
+++ models/staging/salesforce/stg_salesforce__accounts.sql
@@ -1,12 +1,12 @@
 with
 
-    enriched_accounts as (select * from {{ ref ("base_salesforce__accounts_with_foreign_key_names") }} ), 
+    base_records as (select * from {{ ref ("base_salesforce__accounts_with_foreign_key_names") }} ), 
 
-    excluding_deleted_records as (select * from enriched_accounts where is_deleted = false),
+    excluding_deleted_records as (select * from base_records where is_deleted = false),
 
     snake_case_field_names_and_clean_timestamps as (
         -- Original command was using macro and then compiled output shown here for consistency and visibilty of SF Field Aliases
-        -- Original command was:   select {% raw %}{{ select_fields_structured(ref('base_salesforce__accounts_enriched')) }}{% endraw %} from excluding_deleted_records
+        -- Original command was:   select {% raw %}{{ select_fields_structured(ref('base_salesforce__accounts_with_foreign_key_names')) }}{% endraw %} from excluding_deleted_records
        
 select -- Identifiers,
 
